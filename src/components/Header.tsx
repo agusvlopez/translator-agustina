1434049cdea41c404eb95a503b638c931f8d135f
agusvlopez <agusvlopez@gmail.com>
2025-09-04 23:51:19 -0300

    Add: Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import { useTranslation } from 'react-i18next';
+import './Header.css';
+
+export function Header () {
+  const { t } = useTranslation();
+
+  return (
+    <header className="header">
+      <div className="header-content">
+        <h1>
+          <span className="header-title">{t('hero.title')}</span>
+          <span className="header-subtitle">{t('hero.subtitle')}</span>
+        </h1>
+        <p className="header-description">{t('hero.description')}</p>
+        {/* TODO: CHEQUEAR SI AGREGO ESTE BOTON */}
+        {/* <BaseButton >{t('cta.services')}</BaseButton> */}
+      </div>
+    </header>
+  );
+}
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,13 +1,16 @@
 import { Navbar } from '../components/Navbar';
 import { About } from '../components/About';
+import { Header } from '../components/Header';
 
 function LandingPage() {
   
   return (
     <div className="landing-page">
       {/* Selector de idioma */}
-      <Navbar />
-      
+      <div className="header-container">
+        <Navbar />
+        <Header />
+      </div>
       <About />
 
   
