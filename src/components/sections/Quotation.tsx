8326122f32bfd2412d13de8cd922c7877f95fe6b
agusvlopez <agusvlopez@gmail.com>
2025-09-12 11:53:55 -0300

    Fix: Styling

--- src/components/sections/Quotation.tsx
+++ src/components/sections/Quotation.tsx
@@ -14,7 +14,7 @@ export function Quotation() {
         <h2 className="quotation-title">{t('quotation.title')}</h2>
         <p className="quotation-description">{t('quotation.description')}</p>
         <div className="quotation-buttons">
-          <BaseButton isMailto mailtoUrl={mailtoUrl} className="quotation-button">{t('quotation.button')}</BaseButton>
+          <BaseButton isMailto mailtoUrl={mailtoUrl} className="quotation-button" variant="light">{t('quotation.button')}</BaseButton>
           <Link to="/services" className="services-button" aria-label={t('cta.services')}>
             {t('cta.services')}
             <svg width="24" height="24" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
